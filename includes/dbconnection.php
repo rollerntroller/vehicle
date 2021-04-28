a0b133c9e0a17608a5bdf18275d5019856f8b505
karadiakhil <karadiakhil@gmail.com>
2021-04-28 18:15:15 +0100

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "vpmsdb");
+$con=mysqli_connect("dublinvehicl.cy0nam0gijjl.us-east-1.rds.amazonaws.com", "admin", "admin123", "vpmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
