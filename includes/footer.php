19a3e0a131b6d02093fb62465ab64bb9f8f0ab72
karadiakhil <karadiakhil@gmail.com>
2021-04-30 14:55:55 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
             <div class="footer-inner bg-white">
                 <div class="row">
                     <div class="col-sm-6">
-                        Copyright &copy; 2019
+                        Copyright &copy; x20152868@student.ncirl.ie
                     </div>
                     <div class="col-sm-6 text-right">
                         Vehicle Parking Management System
